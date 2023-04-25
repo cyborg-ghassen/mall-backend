e874ac6ec9cc10522b761efb196cce93919792c6
cyborg <ghassen.bougacha@gmail.com>
2023-04-26 00:40:16 +0100

    update s3 and python 3.9

--- brand/models.py
+++ brand/models.py
@@ -16,6 +16,7 @@ class ClothingItem(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="clothing", null=True, blank=True)
 
     def __str__(self):
         return self.name
