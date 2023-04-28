8dc0cfb4079d33de5f9f067874e8332b3472aa95
cyborg <ghassen.bougacha@gmail.com>
2023-04-28 22:42:31 +0100

    update

--- restaurant/models.py
+++ restaurant/models.py
@@ -17,6 +17,7 @@ class MenuItem(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to="restaurants/menus/", null=True, blank=True)
 
     def __str__(self):
         return self.name
