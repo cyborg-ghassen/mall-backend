7f8492d3cc257b778840a768a722ce78234a2bb3
cyborg <ghassen.bougacha@gmail.com>
2023-04-30 17:44:16 +0100

    update

--- electronic/models.py
+++ electronic/models.py
@@ -11,10 +11,19 @@ class ElectronicsShop(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
 class ElectronicsItem(models.Model):
     name = models.CharField(max_length=100)
-    shop = models.ForeignKey(ElectronicsShop, on_delete=models.CASCADE)
+    shop = models.ForeignKey(ElectronicsShop, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField()
+    image = models.ImageField(upload_to="shops/items/", null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
