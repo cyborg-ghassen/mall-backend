05ac0f41c7ac3dfd7b9689e6def516a58404bbb7
cyborg <ghassen.bougacha@gmail.com>
2023-04-30 17:47:48 +0100

    update

--- electronic/urls.py
+++ electronic/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 routes = DefaultRouter()
 routes.register(r'shop', ShopViewSet, basename='shop')
+routes.register(r'category', CategoryViewSet, basename='category')
 routes.register(r'item', ItemViewSet, basename='item')
 
 
